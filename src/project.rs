aadb5034750b2639825a8b7e7ff56e06670da4c0
Sakooooo <sakosakosako3times@gmail.com>
2026-05-28 14:31:55 +0300

    basic inital project creation

--- src/project.rs
+++ src/project.rs
@@ -1,15 +1,36 @@
 use std::{fs, path::Path};
 
 use log::{error, info, log};
+use serde::{Deserialize, Serialize};
 
+#[derive(Serialize, Deserialize)]
 struct Project {
     name: String,
-    version: u32,
+    version: String,
+}
+
+impl Default for Project {
+    fn default() -> Self {
+        Self {
+            name: String::from("My cool website"),
+            version: String::from(env!("CARGO_PKG_VERSION")),
+        }
+    }
 }
 
 pub fn init(directory: Option<String>) {
     let project_file = if let Some(directory) = &directory {
-        Path::new(directory).join("wot.toml")
+        let target = Path::new(directory);
+        if !target.exists() {
+            match std::fs::create_dir(&target) {
+                Ok(_) => info!("Creating directory {}", directory),
+                Err(e) => {
+                    error!("Failed to create directory! {}", e);
+                    return;
+                }
+            };
+        }
+        target.join("wot.toml")
     } else {
         Path::new("wot.toml").to_path_buf()
     };
@@ -18,5 +39,19 @@ pub fn init(directory: Option<String>) {
         error!("A project already exists here!");
         info!("hint: remove wot.toml");
         return;
+    };
+
+    let default = Project::default();
+    let contents = match toml::to_string_pretty(&default) {
+        Ok(contents) => contents,
+        Err(e) => {
+            error!("Failed to turn default values into wot.toml! {}", e);
+            return;
+        }
+    };
+
+    match std::fs::write(project_file, contents) {
+        Ok(_) => info!("Successfully initalized project!"),
+        Err(e) => error!("Failed to write project file {}", e),
     };
 }
